cdf322b2d563c9d193a1cb74e514195243713bf6
Alex <xrlogic1@gmail.com>
2024-02-21 21:43:14 +0400

    *

---
+++ Sockets_Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+Console.WriteLine("Enter nickname to connect");
+
+try
+{
+    client.Connect(IPEndPoint.Parse("127.0.0.1:13374"));
+}
+catch (SocketException ex)
+{
+    Console.WriteLine(ex.Message);
+
+    return;
+}
+
+Task.Run(() =>
+{
+    try
+    {
+        while (true)
+        {
+            var buffer = new byte[1024];
+
+            client.Receive(buffer);
+
+            var message = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine($"Message from server: {message}");
+        }
+    }
+    catch (SocketException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        client.Dispose();
+    }
+});
+
+while (true)
+{
+    string message = Console.ReadLine()!;
+
+    if (message == "0")
+    {
+        client.Disconnect(false);
+        break;
+    }
+
+    byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+    client.Send(bytes);
+}
---
+++ Sockets_Server/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+using var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+var endPoint = IPEndPoint.Parse("127.0.0.1:13374");
+
+server.Bind(endPoint);
+server.Listen();
+
+var clients = new List<Socket>();
+
+while (true)
+{
+    var client = server.Accept();
+
+    clients.Add(client);
+
+    Console.WriteLine($"Client accepted {clients.Count}");
+
+    Task.Run(() =>
+    {
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            int byteLength = 0;
+            while ((byteLength = client.Receive(buffer)) > 0)
+            {
+                var message = Encoding.UTF8.GetString(buffer);
+                var reversed = message.Reverse();
+                var builder = new StringBuilder();
+
+                foreach (var symbol in reversed)
+                {
+                    builder.Append(symbol);
+                }
+
+                Console.WriteLine($"[{byteLength}]: {message}");
+
+                var response = Encoding.UTF8.GetBytes(builder.ToString());
+
+                client.Send(response);
+                Array.Clear(buffer, 0, byteLength);
+            }
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine("Client disconnected!");
+        }
+        finally
+        {
+            client.Dispose();
+        }
+    });
+}
