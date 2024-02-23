50762966d6f8968cf2f8f457d37c30d656e07df2
Alex <xrlogic1@gmail.com>
2024-02-23 20:46:30 +0400

    TCP/IP

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var client = new TcpClient(IPEndPoint.Parse("127.0.0.1:13375")); // my endpoint
+
+
+client.Connect("127.0.0.1", 13374); // remote endpoint
+
+var stream = client.GetStream();
+var buffer = new byte[1024];
+
+while (true)
+{
+    Console.WriteLine("Enter a message: ");
+    var message = Console.ReadLine()!;
+    var request = Encoding.UTF8.GetBytes(message);
+    
+    stream.Write(request);
+    stream.Read(buffer, 0, buffer.Length);
+
+    var response = Encoding.UTF8.GetString(buffer);
+
+    Console.WriteLine(response);
+}
